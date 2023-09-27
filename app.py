0f75a63aece8dbc1990ab214e941cdf68742e3ea
dangvue0 <dang.vue0@gmail.com>
2023-09-27 12:04:42 -0500

    added jwt

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 import json
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
 import pickle
 import numpy as np
 import pandas as pd
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "ml_secret_key"  # Change this!
+jwt = JWTManager(app)
+
+# curl -X POST http://127.0.0.1:5000/login -H "Content-type: application/json" -d "{\"username\" : \"dang\", \"password\" : \"vue\"}"
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "dang" or password != "vue":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+
+
 
 @app.route('/')
 def index():
@@ -27,12 +51,16 @@ def predict():
 
 
 # ADDING NEW ENDPOINT
-#     curl -X POST -H "Content-type: application/json" -d "{\"Sepal_Length\" : 5.1, \"Sepal_Width\" : 3.5, \"Petal_Length\" : 1.4, \"Petal_Width\" : 0.2}" "localhost:5000/api/proba"
+#     curl -X POST localhost:5000/api/proba -H "Content-type: application/json" -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY5NTc1MzA5NCwianRpIjoiZTIzZDVhYWUtYTBhMi00OWRmLWE0NWUtNTNhY2IxZDNiOTEzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImRhbmciLCJuYmYiOjE2OTU3NTMwOTQsImV4cCI6MTY5NTc1Mzk5NH0.HKv1kYnEMwL6fVZC6UDKpll-J8S3U9FKZBEOHTk2tac" -d "{\"Sepal_Length\" : 5.1, \"Sepal_Width\" : 3.5, \"Petal_Length\" : 1.4, \"Petal_Width\" : 0.2}" 
 
 @app.route('/api/proba', methods=['POST'])
+@jwt_required()
 def proba():
 
     print("This")
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    print(current_user)
 
     data = request.get_json()
     json_data = json.dumps(data)
@@ -43,7 +71,7 @@ def proba():
     _proba = model.predict_proba(dfreadjson)[:,1]
     print(prediction)
 
-    return str(_proba), 200
+    return jsonify(model.predict_proba(dfreadjson)[:,1].tolist()), 200
 
 
 if __name__ == '__main__':
