f2ea0fa76042166e67232fa5b10c103643c0856f
dangvue0 <86640011+dangvue0@users.noreply.github.com>
2023-10-06 10:31:49 -0500

    Update test.py
    #change class function call to self

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 from parsedata import ParseData
 
 data = {
-    "score" : 750,
-    "tm_screen_res" : "1920x1080",
-    "email_addr" : "dang.vue0@gmail.com",
-    "ip_address" : "192.168.0.1"
-    }
+"score" : 750,
+"tm_screen_res" : "1920x1080",
+"email_addr" : "dang.vue0@gmail.com",
+"ip_address" : "192.168.0.1"
+}
 
-p = ParseData(data)
-p1 = p.parse_screen_res()
-p2 = p.parse_email()
-p3 = p.parse_ip()
+p = ParseData()
+p1 = p.parse_screen_res(data)
+p2 = p.parse_email(data)
+p3 = p.parse_ip(data)
 print(p1, type(p1), p2, type(p2), p3, type(p3))
 
-pAll = ParseData(data).parse()
+pAll = ParseData().parse(data)
 print(pAll)
