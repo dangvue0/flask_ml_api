8b45295aed530df97ab2e8ca2902b0394ae83f8e
dangvue0 <86640011+dangvue0@users.noreply.github.com>
2023-10-11 12:59:59 -0500

    Update parsedata.py
    update to parsedata with init

--- parsedata.py
+++ parsedata.py
@@ -1,17 +1,17 @@
 import re
 
-class ParseData:
-
-    def parse_screen_res(self, data):
-        self.data = data
+class ParseData(object):
+    def __init__(self, data):
+         self.data = data
+        
+    def parse_screen_res(self):
         if (isinstance(self.data["tm_screen_res"],(int, float))):
             return self.data["tm_screen_res"]
         else:
             match = re.findall('\d+$', self.data["tm_screen_res"])
             return int(match[0])
     
-    def parse_email(self, data):
-        self.data = data
+    def parse_email(self):
         match = re.findall('(?<=@)[\w+]+', self.data["email_addr"])
         # insert email labelencoder here
 
@@ -19,17 +19,16 @@ class ParseData:
 
         return match[0]
         
-    def parse_ip(self, data):
-        self.data = data
+    def parse_ip(self):
         if (isinstance(self.data["ip_address"],(int, float))):
             return self.data["ip_address"]
         else:
             match = re.findall('^\d+', self.data["ip_address"])
             return int(match[0])
     
-    def parse(self, data):
-        self.data = data
-        self.data["tm_screen_res"] = self.parse_screen_res(self.data)
-        self.data["email_addr"] = self.parse_email(self.data)
-        self.data["ip_address"] = self.parse_ip(self.data)
+    def parse(self):
+        self.data["tm_screen_res"] = self.parse_screen_res()
+        self.data["email_addr"] = self.parse_email()
+        self.data["ip_address"] = self.parse_ip()
         return self.data
+        
